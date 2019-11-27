cf90b8f1c692fb37d08b0a686d99fd182b99adce
Stephen Kong <stephen.kong@managebac.com>
2019-11-27 20:34:59 +0800

    Access Jira

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+extern crate reqwest;
+
 use structopt::StructOpt;
+use reqwest::header::AUTHORIZATION;
+use reqwest::header::CONTENT_TYPE;
 
 #[derive(StructOpt)]
 struct Cli {
-    issue_id: u32
+    issue: String
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    println!("{}", args.issue_id);
+    let reqeust_url = format!(
+        "https://fariaedu.atlassian.net/rest/api/latest/issue/{issue}", 
+        issue=&args.issue);
+    let client = reqwest::Client::new();
+    let resp = client.get(&reqeust_url)
+        .header(AUTHORIZATION, "Basic c3RlcGhlbi5rb25nQG1hbmFnZWJhYy5jb206NThvWkV2V1ZMekRtcjNETVhvQUk1QzVF")
+        .header(CONTENT_TYPE, "application/json")
+        .send()?
+        .text()?;
+
+    println!("{}", resp);
+
+    Ok(())
 }
